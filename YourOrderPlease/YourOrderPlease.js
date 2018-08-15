40555362c8b963062ff4a6773f6505862b336c71
Nick Bansal <extreme_wired@msn.com>
2018-08-15 15:03:46 +0100

    Your order test

---
+++ YourOrderPlease/YourOrderPlease.js
@@ -0,0 +1,38 @@
+function order(words){
+	var newArr = words.split(' ');
+	var result = [];
+	for (let j=0; j<=newArr.length; j++) {
+		for (let i=0; i<newArr.length; i++) {
+			if (newArr[i].indexOf(j) >= 0) {
+				result.push(newArr[i])
+			}
+		}
+	}	
+	return result.join(' ');
+}
+
+
+
+console.log(order("is2 Thi1s T4est 3a"))
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
