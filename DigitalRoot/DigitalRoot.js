05127a44196884830e72b41b2a1c815417bc0dd9
Nick Bansal <extreme_wired@msn.com>
2018-08-15 12:21:56 +0100

    Final Digital Root

--- DigitalRoot/DigitalRoot.js
+++ DigitalRoot/DigitalRoot.js
@@ -1,7 +1,8 @@
-function digital_root(n) {
-  var num = n;
+function digital_root(num) {
   while (num > 9) {
   	num = num.toString().split('').map(Number).reduce((x,i) => x+i)
   }
   return num
 }
+
+console.log(digital_root(16))
