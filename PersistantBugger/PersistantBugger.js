d09b7cbb21e15bd6b78faf6cb4b8eff16b33c116
NickBansal <extreme_wired@msn.com>
2018-08-15 10:54:19 +0100

    Update PersistantBugger.js

--- PersistantBugger/PersistantBugger.js
+++ PersistantBugger/PersistantBugger.js
@@ -1,9 +1,8 @@
 function persistence(num) {
 	var count = 0
 	while (num > 9) {
-		var numStr = num.toString().split('');
-		var newStr = numStr.reduce((x,i) => x*i);
-		num = Number(newStr);
+		var numStr = num.toString().split('').reduce((x,i) => x*i);
+		num = Number(numStr);
 		count++;
 	}
  	return count
