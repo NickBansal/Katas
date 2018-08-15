cc00854c3fe71dd9a4236b894842f5c3961aba4f
Nick Bansal <extreme_wired@msn.com>
2018-08-15 10:53:34 +0100

    Persistant Bugger 2

--- PersistantBugger/PersistantBugger.js
+++ PersistantBugger/PersistantBugger.js
@@ -1,12 +1,11 @@
 function persistence(num) {
 	var count = 0
 	while (num > 9) {
-		var numStr = num.toString().split('');
-		var newStr = numStr.reduce((x,i) => x*i);
-		num = Number(newStr);
+		var numStr = num.toString().split('').reduce((x,i) => x*i);
+		num = Number(numStr);
 		count++;
 	}
  	return count
 }
 
-console.log(persistence(4));
+console.log(persistence(999));
