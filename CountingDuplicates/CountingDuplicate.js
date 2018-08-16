c2a13e7fc5f496a784124b61519843e5601a6123
Nick Bansal <extreme_wired@msn.com>
2018-08-16 09:35:29 +0100

    You're a Square

--- CountingDuplicates/CountingDuplicate.js
+++ CountingDuplicates/CountingDuplicate.js
@@ -1,10 +1,9 @@
 function duplicateCount(text){
 	return text
-	.toLowerCase()
 	.split('')
-	.sort()
-	.filter((x, index, arr) => (arr.indexOf(x) !== index ))
+	.filter((x, index, arr) => arr.indexOf(x) !== index)
 	.filter((x, index, arr) => arr.indexOf(x) === index).length
 }
 
-console.log(duplicateCount('AbbA'))
+console.log(duplicateCount('indivXMXisibilitysLLssstttt'))
+
