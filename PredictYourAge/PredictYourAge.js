f41c65ac5ca61a3b3a2af362502b459f42408238
Nick Bansal <extreme_wired@msn.com>
2018-08-16 10:10:24 +0100

    refactored

--- PredictYourAge/PredictYourAge.js
+++ PredictYourAge/PredictYourAge.js
@@ -1,8 +1,9 @@
 function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
-	var result = ((age1 * age1)+(age2 * age2)+(age3 * age3)+(age4 * age4)+
-	(age5 * age5)+(age6 * age6)+(age7 * age7)+(age8 * age8))
-	var squareRoot = Math.pow(result, 0.5);
-	return Math.floor(squareRoot/2);
+	return Math.floor(Math.pow(
+		[age1,age2,age3,age4,age5,age6,age7,age8]
+		.map(x => x*x)
+		.reduce((x,index) => x+index), 0.5)/2)
 }
+
 
 
