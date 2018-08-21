51c77368c92468aea84fb674c980418b8ac75479
Nick Bansal <extreme_wired@msn.com>
2018-08-21 13:46:35 +0100

    Final Valid

--- ValidParentheses/ValidParentheses.js
+++ ValidParentheses/ValidParentheses.js
@@ -1,10 +1,15 @@
 function validParentheses(parens) {
-	return (
-		parens.split('').filter(x => x === '(').length === 
-		parens.split('').filter(x => x === ')').length) ? 
-	true : false	
+	var split = parens.split('')
+	if (split[0] === '(' && split[split.length - 1] === ')' && split.length > 1) {
+		return (
+			parens.split('').filter(x => x === '(').length === 
+			parens.split('').filter(x => x === ')').length) ? 
+		true : false
+	} else {
+		return false;
+	}
 }
 
 
 
-console.log(validParentheses("()()()()()()()()"))
+console.log(validParentheses(")(()"))
